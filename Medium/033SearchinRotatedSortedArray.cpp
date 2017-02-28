424c103cff9a9705074646ab55a408b147074dce
animebing <yanbing_dong@hotmail.com>
2017-02-28 10:26:14 +0800

    Search in Rotated Sorted Array

---
+++ Medium/033SearchinRotatedSortedArray.cpp
@@ -0,0 +1,32 @@
+// first find the place where the array is rotated, then use binary searth
+// O(n)
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int r = 0;
+        for (int i = 0; i < n-1; i++) {
+            if (nums[i] > nums[i+1]) {
+                r = i+1;
+                break;
+            }
+        }
+        
+        int low = 0, high = n-1;
+        
+        while (low <= high) {
+            int middle = low + (high-low)/2;
+            int idx = (middle+r)%n;
+            
+            if (nums[idx] == target) return idx;
+            else if (nums[idx] > target) high = middle-1;
+            else low = middle+1;
+        }
+        
+        return -1;
+    }
+};
+
+
+// there are some solution to use the fact that the array partly sorted, so O(logn), but i can't understand their solution
+// i will see these solution later 
