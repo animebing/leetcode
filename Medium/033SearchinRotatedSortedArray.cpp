5b19f030fe7b84698108484f210bca8539c90aab
animebing <yanbing_dong@hotmail.com>
2017-03-13 10:30:22 +0800

    add another solution

--- Medium/033SearchinRotatedSortedArray.cpp
+++ Medium/033SearchinRotatedSortedArray.cpp
@@ -30,3 +30,24 @@ public:
 
 // there are some solution to use the fact that the array partly sorted, so O(logn), but i can't understand their solution
 // i will see these solution later 
+
+// use binary search directly, but consider different situations when nums[mid] != target
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        if (nums.size() == 0) return -1;
+        int low = 0, high = nums.size()-1;
+        while (low <= high) {
+            int mid = low + (high-low)/2; // mid = low&high + (low^high)>>1;
+            if (nums[mid] == target) return mid;
+            if (nums[mid] < nums[high]) {
+                if (target>nums[mid] && target<=nums[high]) low = mid+1;
+                else high = mid-1;
+            }else {
+                if (target>=nums[low] && target<nums[mid]) high = mid-1;
+                else low = mid+1;
+            }
+        }
+        return -1;
+    }
+};
