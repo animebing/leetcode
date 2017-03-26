9053502e5d213094545e77a62c6d576370fb00f1
animebing <yanbing_dong@hotmail.com>
2017-03-26 09:05:42 +0800

    change the boarder condition

--- Medium/130SurroundedRegions.cpp
+++ Medium/130SurroundedRegions.cpp
@@ -36,3 +36,38 @@ private:
 
 // the solution of https://discuss.leetcode.com/topic/2982/my-bfs-solution-c-28ms is BFS version 
 // there are find answer using union find, I don't know what it is now
+
+
+// I make some change, but still run time error, it is ok now
+class Solution {
+public:
+    void solve(vector<vector<char>>& board) {
+        if (board.empty()) return;
+        int m = board.size(), n = board[0].size();
+        for (int col = 0; col < n; col++) {
+            if (board[0][col] == 'O') check(board, 0, col); 
+            if (board[m-1][col] == 'O') check(board, m-1, col);
+        }
+
+        for (int row = 1; row <= m-2; row++) {
+            if (board[row][0] == 'O') check(board, row, 0);
+            if (board[row][n-1] == 'O') check(board, row, n-1);
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'O') board[i][j] = 'X';
+                else if (board[i][j] == 'S') board[i][j] = 'O';
+            }
+        }
+    }
+private:
+    void check(vector<vector<char>> &board, int row, int col) {
+        int m = board.size(), n = board[0].size();
+        board[row][col] = 'S';
+        if (row-1>=1 && board[row-1][col]=='O') check(board, row-1, col); // i change the boarder condition
+        if (row+1<m-1 && board[row+1][col]=='O') check(board, row+1, col);
+        if (col-1>=1 && board[row][col-1]=='O') check(board, row, col-1);
+        if (col+1<n-1 && board[row][col+1]=='O') check(board, row, col+1);
+        
+    }
+};
